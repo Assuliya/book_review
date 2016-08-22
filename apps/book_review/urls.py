e605aae18038cf044f4b830f3363f4c092303d67
Assuliya <t.nesterchuk@mail.ru>
2016-08-22 18:24:05 +0300

    models. user set

--- apps/book_review/urls.py
+++ apps/book_review/urls.py
@@ -3,9 +3,13 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'books/add$', views.add, name='add'),
-    url(r'user$', views.user, name='user'),
+    url(r'user/(?P<user_id>\d+)$', views.user, name='user'),
     url(r'books$', views.books, name='books'),
-    url(r'books/specific$', views.specific, name='specific')
+    url(r'books/specific$', views.specific, name='specific'),
+
+    url(r'^user/login_process$', views.login_process, name='login_process'),
+    url(r'^user/register_process$', views.register_process, name='register_process'),
+    url(r'^user/logout$', views.logout, name='logout')
 
 
 ]
--- apps/book_review/views.py
+++ apps/book_review/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
-# from models import User, Message, Comment
+from models import User, Book, Review
 import bcrypt
 
 def index(request):
@@ -10,14 +10,50 @@ def index(request):
 def add(request):
     return render(request, 'book_review/add.html')
 
-def user(request):
-    return render(request, 'book_review/user.html')
+def user(request, user_id):
+    user = User.objects.get(id = user_id)
+    context = {'user':user}
+    return render(request, 'book_review/user.html', context)
 
 def books(request):
 
     return render(request, 'book_review/books.html')
 
 def specific(request):
-    # user = User.objects.get(id = user_id)
-    # context = {'user':user}
+
     return render(request, 'book_review/specific.html')
+
+
+
+def register_process(request):
+    result = User.manager.validateReg(request)
+    resultPass = User.manager.validateRegPass(request)
+    if result[0] == False or resultPass[0] == False:
+        errors = result[1]+resultPass[1]
+        print_messages(request, errors)
+        return redirect(reverse('index'))
+    pw_hash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+    user = User.manager.create(name=request.POST['name'], alias=request.POST['alias'], email=request.POST['email'], pw_hash=pw_hash)
+    return log_user_in(request, user)
+
+def login_process(request):
+    result = User.manager.validateLogin(request)
+    if result[0] == False:
+        print_messages(request, result[1])
+        return redirect(reverse('login'))
+    return log_user_in(request, result[1])
+
+def print_messages(request, message_list):
+    for message in message_list:
+        messages.add_message(request, messages.ERROR, message)
+
+def log_user_in(request, user):
+    request.session['user'] = user.id
+    return redirect(reverse('user', kwargs={'user_id':request.session['user']}))
+
+def logout(request):
+    user = User.manager.get(id=request.session['user'])
+    user.user_level = 0
+    user.save(update_fields=None)
+    request.session.pop('user')
+    return redirect(reverse('index'))
